a86ae278411f7af8e54062ac56b85b39eba24a94
Darshitpipariya <darshit2272000@gmail.com>
2022-11-29 19:05:57 +0530

    Complete Project

--- src/main/java/com/example/backend_payment/HelloApplication.java
+++ src/main/java/com/example/backend_payment/HelloApplication.java
@@ -1,9 +1,16 @@
 package com.example.backend_payment;
-
+import com.example.backend_payment.Util.CORSFilter;
 import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.core.Application;
-
+import org.glassfish.jersey.server.ResourceConfig;
 @ApplicationPath("/api")
-public class HelloApplication extends Application {
+public class HelloApplication extends ResourceConfig {
+    public HelloApplication(){
+        // Registering the CORSFilter class with the Jersey ResourceConfig
+        register(CORSFilter.class);
+
+        // Telling Jersey the CLASSPATH where the specified classes (in our case, CORSFilter) can be found
+        packages("com.example.backend_payment");
+    }
 
 }
