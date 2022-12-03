5a2e67e9a6ca07a1cad951a5125ace7c28141579
Darshitpipariya <darshit2272000@gmail.com>
2022-12-03 12:12:01 +0530

    Complete Project

--- src/main/java/com/example/backend_payment/DAO/DAOImplementation/Student_PaymentDAOImplementation.java
+++ src/main/java/com/example/backend_payment/DAO/DAOImplementation/Student_PaymentDAOImplementation.java
@@ -17,8 +17,8 @@ public class Student_PaymentDAOImplementation implements Student_PaymentDAO {
         try(Session session= HibernateSessionUtil.getSession()){
             Transaction transaction=session.beginTransaction();
             Bills bills=session.get(Bills.class,student_payment.getBill().getBill_id());
-            bills.setRemaining(bills.getRemaining()-student_payment.getAmount());
             student_payment.setRemaining(bills.getRemaining()-student_payment.getAmount());
+            bills.setRemaining(bills.getRemaining()-student_payment.getAmount());
             session.saveOrUpdate(bills);
             session.saveOrUpdate(student_payment);
             transaction.commit();
